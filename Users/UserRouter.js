846be26302ecd840592018c543ce32fdea35d405
dgrade <gradeneggerdominic@gmail.com>
2020-02-27 15:19:43 +0100

    API Routers created

---
+++ Users/UserRouter.js
@@ -0,0 +1,43 @@
+/*
+
+HTTP Endpoints for the user - REST API
+
+Method  |   url
+
+GET     |   /
+GET     |   /:id
+POST    |   /
+PUT     |   /:id
+PATCH   |   /:id
+DELETE  |   /:id
+
+*/
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.status(500).send("Not implemented");
+});
+
+router.get('/:id', (req, res) => {
+    res.status(500).send("Not implemented");
+});
+
+router.post('/', (req, res) => {
+    res.status(500).send("Not implemented");
+});
+
+router.put('/:id', (req, res) => {
+    res.status(500).send("Not implemented");
+});
+
+router.patch('/:id', (req, res) => {
+    res.status(500).send("Not implemented");
+});
+
+router.delete('/:id', (req, res) => {
+    res.status(500).send("Not implemented");
+});
+
+module.exports = router;

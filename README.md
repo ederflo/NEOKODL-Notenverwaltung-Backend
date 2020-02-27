700dd37778cd0f4602bde4c669ca10f6e106f70d
dgrade <gradeneggerdominic@gmail.com>
2020-02-27 11:24:17 +0100

    Express Setup
    hostname und port nochmals anschauen und ändern

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 NEOKODL-Notenverwaltung
+
+hostname: localhost
+port: 5000
---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const hostname = "localhost";
+const port = "5000";
+
+const app = express();
+connectToDb();
+
+async function connectToDb(){
+    try {
+        //connection aufbauen
+
+        defaultSetup();
+    } catch (err) {
+        errorSetup(err);
+    }
+}
+
+function defaultSetup(){
+    app.use(bodyparser.json());
+    app.get("/", (req, res) => {res.send("Express is up and running.")});
+
+    app.listen(port, hostname, () => {console.log(`Express is up and running on ${hostname}:${port}`)});
+}
+function errorSetup(err){
+    app.use('*', (req, res) => {res.status(500).send("An error occured.")});
+    app.listen(port, hostname, () => {
+        console.error("An error occured: " + err);
+    });
+}

eb122281f57a58ac1912a41508bce5b0ea593d24
Florian Eder <ederflo@gmx.net>
2020-03-24 16:24:47 +0100

    Renewed config management, fixed api urls in tests, installed and set up cors

---
+++ Services/configManager.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+const configManager = {};
+const defaultConfigPath = './../configs/default.json';
+
+let configData = {}
+
+configManager.loadConfig = function(filepath) {
+    if (!filepath || filepath.length <= 0 || !fs.existsSync(filepath))
+        filepath = defaultConfigPath;
+    configData = require(filepath);
+}
+
+configManager.getData = function() {
+    if (Object.keys(configData).length <= 0) {
+        this.loadConfig(defaultConfigPath);
+    }
+    return configData;
+}
+
+configManager.getDataBaseConfig = function() {
+    if (Object.keys(configData).length <= 0 || Object.keys(configData['dbSettings']).length <= 0) {
+        this.loadConfig(defaultConfigPath);
+    }
+    return configData['dbSettings'];
+}
+
+configManager.getBackendData = function() {
+    console.log(Object.keys(configData).length <= 0 + ' ' + Object.keys(configData['backend']).length <= 0)
+    if (Object.keys(configData).length <= 0 || Object.keys(configData['backend']).length <= 0) {
+        this.loadConfig(defaultConfigPath);
+    }
+    return configData['backend'];
+}
+
+configManager.getCorsData = function() {
+    console.log(Object.keys(configData).length <= 0 + ' ' + Object.keys(configData['corsOptions']).length <= 0)
+    if (Object.keys(configData).length <= 0 || Object.keys(configData['corsOptions']).length <= 0) {
+        this.loadConfig(defaultConfigPath);
+    }
+    return configData['corsOptions'];
+}
+
+module.exports = configManager;

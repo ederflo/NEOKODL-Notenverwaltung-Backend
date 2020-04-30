b308d337d6d02e77ea44099f0ca8f2a8b09e4718
dgrade <gradeneggerdominic@gmail.com>
2020-04-30 15:28:53 +0200

    Bugfix Date Format

--- Pupils/PupilsRouter.js
+++ Pupils/PupilsRouter.js
@@ -5,7 +5,7 @@ const AppError = require('../Services/error-management').AppError;
 const handleError = require('../Services/error-management').handleError;
 
 router.get('/', (req, res) => {
-    res.status(200).json([{"id":"1","email":"exampleMail@mail.com","birthdt":"27.04.2020","firstname":"Karli","lastname":"Kolumbus","notes":"Wofür is dieses Feld eigentlich do?"},{"id":"2","email":"exampleNail@nail.com","birthdt":"27.04.2020","firstname":"Karlo","lastname":"Astro","notes":"Die Frage bleibt bestehen."},{"id":"3","email":"exampleXail@xail.com","birthdt":"27.04.2020","firstname":"Karle","lastname":"Kolumbe","notes":"Die Frage."}]);
+    res.status(200).json([{"id":"1","email":"exampleMail@mail.com","birthdt":"2020-04-27","firstname":"Karli","lastname":"Kolumbus","notes":"Wofür is dieses Feld eigentlich do?"},{"id":"2","email":"exampleNail@nail.com","birthdt":"2020-04-27","firstname":"Karlo","lastname":"Astro","notes":"Die Frage bleibt bestehen."},{"id":"3","email":"exampleXail@xail.com","birthdt":"2020-04-27","firstname":"Karle","lastname":"Kolumbe","notes":"Die Frage."}]);
 });
 
 router.get('/:id', selectById, (req, res) => {
